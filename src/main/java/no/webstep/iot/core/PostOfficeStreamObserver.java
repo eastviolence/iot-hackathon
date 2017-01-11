ec1ae937b4fddcc5a0e6f1da5587d22df7b9af2d
Arne Østvold <arne.ostvold@gmail.com>
2017-01-11 19:56:32 +0100

    YOLO

--- src/main/java/no/webstep/iot/core/PostOfficeStreamObserver.java
+++ src/main/java/no/webstep/iot/core/PostOfficeStreamObserver.java
@@ -1,16 +1,38 @@
 package no.webstep.iot.core;
 
 import com.d21s.api.v1.WatchThingResponse;
+import com.google.protobuf.Timestamp;
 import io.grpc.stub.StreamObserver;
+import no.webstep.iot.api.SMSService;
+
+import java.util.Date;
 
 /**
  * Created by arne.ostvold on 11.01.2017.
  */
 public class PostOfficeStreamObserver implements StreamObserver<WatchThingResponse> {
+    private final SMSService smsService;
+
+    public PostOfficeStreamObserver() {
+        smsService = new SMSService();
+    }
+
     @Override
     public void onNext(WatchThingResponse watchThingResponse) {
         boolean objectPresent = watchThingResponse.getStateChanged().getObjectPresent();
+        float temperature = watchThingResponse.getStateChanged().getTemperature();
+        Date received = new Date(watchThingResponse.getTimestamp().getSeconds() * 1000);
+
+        System.out.println("Temperatur er " + temperature);
         System.out.println("State endret til " + objectPresent);
+
+        if(objectPresent){
+            smsService.sendSMS("+4795948929", generateSMSText(temperature, received));
+        }
+    }
+
+    private String generateSMSText(float temperature, Date received) {
+        return String.format("En pakke til deg ankom kl %s.%s. Pakken lagres på %s grader celsius", received.getHours(), received.getMinutes(), temperature);
     }
 
     @Override
--- src/main/java/no/webstep/iot/core/ThingListener.java
+++ src/main/java/no/webstep/iot/core/ThingListener.java
@@ -1,18 +1,6 @@
 package no.webstep.iot.core;
 
-import com.d21s.api.v1.Thing;
-import com.d21s.api.v1.WatchResponse;
-import com.d21s.api.v1.WatchThingResponse;
-import com.d21s.api.v1.events.ThingStateChangeEvent;
-import com.google.protobuf.Timestamp;
-import io.grpc.stub.StreamObserver;
 import no.webstep.iot.disruptive.DisruptiveClient;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-
-import static java.time.temporal.ChronoUnit.DAYS;
 
 /**
  * Created by arne.ostvold on 11.01.2017.
